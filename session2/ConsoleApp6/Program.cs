dc9d31c5b332bed09bd29075a54a8a8343647819
Iman-Mashayekhi <iman_mashayekhi@outlook.com>
2024-11-04 15:24:25 +0330

    [Rounding students score algorithm]

---
+++ session2/ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp6;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+List<Student> students = new List<Student>();
+
+students.Add(new Student() { id = 1, name = "Alice", score = 3 });
+students.Add(new Student() { id = 2, name = "Bob", score = 7.87 });
+students.Add(new Student() { id = 3, name = "Charlie", score = 9.001 });
+students.Add(new Student() { id = 4, name = "David", score = 1 });
+students.Add(new Student() { id = 5, name = "Eve", score = 13 });
+students.Add(new Student() { id = 6, name = "Frank", score =8  });
+students.Add(new Student() { id = 7, name = "Grace", score= 9.5 });
+students.Add(new Student() { id = 8, name = "Issac", score= 8.86 });
+
+foreach (Student student in students)
+{
+	if (Math.Round(student.score) >= 10)
+	{
+		student.passed = true;
+	}
+	else
+	{
+        student.passed = false;
+    }
+}
+
+foreach (var std in students)
+{
+	string result = $"id: {std.id}, name: {std.name}, score:{std.score}, isPassed: {std.passed}";
+    Console.WriteLine(result);
+}
+
+Console.ReadKey();
---
+++ session2/ConsoleApp6/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Student
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public double score { set; get; }
+        public bool passed;
+
+
+    }
+}
